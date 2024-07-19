0d92b7e08b37eb1d6390e0a737ae2336a0a365bb
dejuliansr <demetriojulian99@gmail.com>
2024-07-19 16:16:51 +0700

    upadate tailiwnd

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 module.exports = {
   content: ['./index.html', './contact-us/index.html'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'tech-sas': "url('https://raw.githubusercontent.com/dejuliansr/robotic-sas/main/images/krbai.jpg')",
+        'badaya-sas': "url('https://raw.githubusercontent.com/dejuliansr/robotic-sas/main/images/krsti.jpg')",
+        'rostu': "url('https://raw.githubusercontent.com/dejuliansr/robotic-sas/main/images/krsbi.jpg')",
+        'fitoplankton': "url('https://raw.githubusercontent.com/dejuliansr/robotic-sas/main/images/fitoplankton.jpg')",
+        'robotic': "url('https://raw.githubusercontent.com/dejuliansr/robotic-sas/main/images/cover.png')"
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'Arial', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
